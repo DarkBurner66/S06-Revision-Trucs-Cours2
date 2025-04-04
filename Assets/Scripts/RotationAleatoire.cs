0d84670c5524a12e94d07b52eec6bed25d2a3399
DarkBurner66 <dominik_saint-denis@outlook.com>
2025-04-03 21:36:54 -0400

    ok

--- Assets/Scripts/RotationAleatoire.cs
+++ Assets/Scripts/RotationAleatoire.cs
@@ -7,9 +7,9 @@ public class RotationAleatoire : MonoBehaviour
     void Start()
     {
         // Rotation completement aleatoire
-        // transform.rotation = Random.rotation;
+         transform.rotation = Random.rotation;
 
-        // Rotation aléeatore entre une rangé des valeurs
+        // Rotation alï¿½eatore entre une rangï¿½ des valeurs
         float rotationY = Random.Range(-180f, 180f);
         transform.Rotate(0, rotationY, 0,Space.World);
     }
